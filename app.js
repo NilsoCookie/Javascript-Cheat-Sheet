f293d9490f2f50a83fe06d124fa8a23e51f27515
Nilso <92335002+NilsoCookie@users.noreply.github.com>
2022-08-05 10:05:07 +0200

    Add files via upload

---
+++ app.js
@@ -0,0 +1,56 @@
+// -- Data types and variables -- //
+// -- Variables -- //
+let str = "Coding"; // let is a variable that can be reassigned
+var str2 = " is fun"; // var is a variable that can't be reassigned
+// Data types is a way to store data in a variable.
+
+// String
+var name = "John"; // -text
+
+// Number
+let age = 30; // - number
+
+// Boolean
+var isChild = false; // - true or false
+
+// Undefined
+let car; // - undefined
+
+// Null
+let dog = null; // - nothing
+
+// -- Strings -- //
+// strings are variables that can hold text
+console.log("Hello World"); // prints Hello World to the console
+console.log("Hello World"[10]); // prints the 10th index from Hello World - start from 0
+console.log("Hello World".length); // prints the length of the string
+
+console.log(str); // prints the variable str
+console.log(str[str.length - 1]); // prints the last index of the string
+
+// -- Numbers -- //
+// numbers are variables that works like math
+console.log(10 + 4); // adds 10 and 4
+console.log(10 - 4); // subtracts 10 and 4
+console.log(10 * 4); // multiplies 10 and 4
+console.log(10 / 4); // divides 10 and 4
+console.log(100 / (4 * 5)); // numbers also use pemdas
+console.log(10 % 4); // modulus - returns the remainder of the division
+
+// -- Booleans -- //
+// booleans are variables that works like logic
+console.log(5 > 4); // returns true if 5 is greater than 4
+console.log(5 < 4); // returns false since 5 is not less than 4
+
+// -- Undefined -- //
+// undefined is a variable that has was never defined a value
+console.log(car); // prints undefined because car has not been defined a value
+
+// -- Null -- //
+// null is a variable that has no value
+console.log(dog); // prints null because dog has no value
+
+// NaN is a variable that is not a number
+console.log(NaN); // prints NaN
+
+// -- Variables end -- //
